6ddcf405ab84eaaae9d50ee7fd304effa2bfdab0
kyj91032 <83624600+kyj91032@users.noreply.github.com>
2022-01-05 23:16:48 +0900

    Update queue.c

--- queue.c
+++ queue.c
@@ -2,7 +2,7 @@
 
 from collections import deque  # queue 라이브러리보다 효율적이고 간단해서 그냥 deque 씀
 
-queue = deque()  # 큐 구현을 위해 deque 라이브러리 사용. 큐 공간 생성. queue를 deque 객체로 정의.
+queue = deque()  # 큐 구현을 위해 deque 라이브러리 사용. 큐 공간 생성. queue를 deque 객체로 정의. deque([list]) 리스트를 매개변수로 받아서 시작 데이터 설정 가능.
 
 삽입5 - 삽입2 - 삽입3 - 삽입7 - 삭제 - 삽입1 - 삽입4 - 삭제
 
